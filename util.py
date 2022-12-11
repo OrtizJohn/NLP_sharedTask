774cfb798833dbdad0ee22e186efc327390e8bc8
John A Ortiz <johno2k35@gmail.com>
2022-12-11 15:54:55 -0700

    updated

--- util.py
+++ util.py
@@ -1,10 +1,11 @@
-from datasets import load_dataset
+#from datasets import load_dataset
 import random
 from collections import defaultdict
 from tqdm import tqdm
 from torch import LongTensor
 from typing import List, Dict
-
+import numpy as np
+from numpy import logical_and, sum as t_sum
 
 def get_even_datapoints(datapoints, n):
     random.seed(42)
@@ -31,4 +32,57 @@ def get_snli(train=10000, validation=1000, test=1000):
     test_dataset = get_even_datapoints(snli['test'], test)
 
     return train_dataset, validation_dataset, test_dataset
+
+
+
+def runAllEvaluations_onAllLabels(predicted_labels,true_labels, which_label=1 ):
+
+    precision = precision(predicted_labels,true_labels,which_label)
+    recall = recall(predicted_labels,true_labels,which_label)
+    f1_score = f1_score(predicted_labels,true_labels)
+    print(f"Precision Score {precision}")
+    print(f"Reall Score {recall}")
+    print(f"F1 Score {f1_score}")
+
+def precision(predicted_labels, true_labels, which_label=1):
+    """
+    Precision is True Positives / All Positives Predictions
+    """
+    pred_which = np.array([pred == which_label for pred in predicted_labels])
+    true_which = np.array([lab == which_label for lab in true_labels])
+    denominator = t_sum(pred_which)
+    if denominator:
+        return t_sum(logical_and(pred_which, true_which))/denominator
+    else:
+        return 0.
+
+
+def recall(predicted_labels, true_labels, which_label=1):
+    """
+    Recall is True Positives / All Positive Labels
+    """
+    pred_which = np.array([pred == which_label for pred in predicted_labels])
+    true_which = np.array([lab == which_label for lab in true_labels])
+    denominator = t_sum(true_which)
+    if denominator:
+        return t_sum(logical_and(pred_which, true_which))/denominator
+    else:
+        return 0.
+
+
+def f1_score(
+    predicted_labels: List[int],
+    true_labels: List[int],
+    which_label: int
+):
+    """
+    F1 score is the harmonic mean of precision and recall
+    """
+    P = precision(predicted_labels, true_labels, which_label=which_label)
+    R = recall(predicted_labels, true_labels, which_label=which_label)
+    if P and R:
+        return 2*P*R/(P+R)
+    else:
+        return 0.
+
 
